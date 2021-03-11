494b632c39815320da439ac0559a59e10340c5cd
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-11 12:29:53 +0530

    app implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Heading from "./components/Heading";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading>Default Heading Example</Heading>
+        <Heading type="h1">H1 Heading Example</Heading>
+        <Heading type="h2">H2 Heading Example</Heading>
+        <Heading type="h6">H6 Heading Example</Heading>
+        <Heading type="h7">H7 Heading Example</Heading>
+        {/* <Heading type={123}>H2 Heading Example</Heading> */}
+        {/* <Heading type="h33">H2 Heading Example</Heading> */}
       </header>
     </div>
   );
